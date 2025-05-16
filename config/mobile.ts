cfa41f8cb89b1cb20d832946487db4e1298c6c2b
Koaladeev <60426691+Koaladeev@users.noreply.github.com>
2025-05-16 20:17:55 +0100

    Add files via upload

---
+++ config/mobile.ts
@@ -0,0 +1,35 @@
+import { MainNavItem, SidebarNavItem } from '@/types';
+
+interface MobileConfig {
+  mainNav: MainNavItem[];
+  sidebarNav: SidebarNavItem[];
+}
+
+export const mobileConfig: MobileConfig = {
+  mainNav: [
+    {
+      title: 'Home',
+      href: '/',
+    },
+    {
+      title: 'Movie',
+      href: '/movie',
+    },
+    {
+      title: 'TV',
+      href: '/tv',
+    },
+  ],
+  sidebarNav: [
+    {
+      title: 'Other Features',
+      items: [
+        {
+          title: 'List',
+          href: '/list',
+          items: [],
+        },
+      ],
+    },
+  ],
+};
---
+++ config/site.ts
@@ -0,0 +1,26 @@
+export type SiteConfig = typeof siteConfig;
+
+export const siteConfig = {
+  name: 'Stream TV and Movies Live and Online | Xanax.one',
+  description:
+    'the ultimate online movie streaming website that brings the magic of cinema to your fingertips. With a vast and diverse database, as well as a multitude of exciting features, NgrimTv offers an unparalleled movie-watching experience for film enthusiasts worldwide.',
+  mainNav: [
+    {
+      title: 'Movie',
+      href: '/movie',
+    },
+    {
+      title: 'Anime',
+      href: '/anime',
+    },
+    {
+      title: 'TV',
+      href: '/tv',
+    },
+  ],
+  links: {
+    twitter: '',
+    github: '',
+    enjoytown: '/',
+  },
+};
